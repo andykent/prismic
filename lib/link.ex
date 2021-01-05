9d742993df3f9818f692e1f76e043d40febbae8d
Andy Kent <andy@binaryalchemy.net>
2021-01-05 10:37:51 +0000

    handle unset document links

--- lib/link.ex
+++ lib/link.ex
@@ -10,6 +10,10 @@ defmodule Prismic.Link do
     }
   end
 
+  def build(_link, revision, content_types) do
+    nil
+  end
+
   defp to_content_type(type_str, content_types) do
     type = String.to_existing_atom(type_str)
     unless type in content_types, do: raise(ArgumentError, "unknown")
